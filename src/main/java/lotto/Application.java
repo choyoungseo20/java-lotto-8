37f7b58d6ca0aa5fc982690985f267058818faa9
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 17:28:42 +0900

    feat(Application): 진입점에서 Controller 호출

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,25 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(inputView(), outputView(), lottoService());
+        lottoController.run();
+    }
+
+    private static InputView inputView() {
+        return new InputView();
+    }
+
+    private static OutputView outputView() {
+        return new OutputView();
+    }
+
+    private static LottoService lottoService() {
+        return new LottoService();
     }
 }
