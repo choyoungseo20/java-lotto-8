d1f9d9b5123b6f67bf2e3c2609a561c8814f9d40
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 19:33:18 +0900

    refactor(WinningResultTest): Parameterized Test를 활용하여 테스트 코드 추가

--- src/test/java/lotto/model/WinningResultTest.java
+++ src/test/java/lotto/model/WinningResultTest.java
@@ -1,16 +1,20 @@
 package lotto.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinningResultTest {
 
-    @DisplayName("당첨 개수가 맞는지 확인한다.")
-    @Test
-    void 당첨_개수가_맞는지_확인한다() {
-        WinningResult winningResult = new WinningResult();
+    private WinningResult winningResult;
+
+    @BeforeEach
+    void setUp() {
+        winningResult = new WinningResult();
 
         LottoRank lottoRank1 = LottoRank.of(5, false);
         LottoRank lottoRank2 = LottoRank.of(5, true);
@@ -19,25 +23,25 @@ public class WinningResultTest {
         winningResult.put(lottoRank1);
         winningResult.put(lottoRank2);
         winningResult.put(lottoRank3);
+    }
 
-        assertThat(winningResult.getCountByRank(LottoRank.FIRST)).isEqualTo(0);
-        assertThat(winningResult.getCountByRank(LottoRank.SECOND)).isEqualTo(1);
-        assertThat(winningResult.getCountByRank(LottoRank.THIRD)).isEqualTo(2);
+    @DisplayName("당첨 개수가 맞는지 확인한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "FIRST, 0",
+            "SECOND, 1",
+            "THIRD, 2",
+            "FOURTH, 0",
+            "FIFTH, 0",
+            "NONE, 0"
+    })
+    void 당첨_개수가_맞는지_확인한다(LottoRank lottoRank, int expected) {
+        assertThat(winningResult.getCountByRank(lottoRank)).isEqualTo(expected);
     }
 
     @DisplayName("총 당첨 금액이 맞는지 확인한다.")
     @Test
     void 총_당첨_금액이_맞는지_확인한다() {
-        WinningResult winningResult = new WinningResult();
-
-        LottoRank lottoRank1 = LottoRank.of(5, false);
-        LottoRank lottoRank2 = LottoRank.of(5, true);
-        LottoRank lottoRank3 = LottoRank.of(5, false);
-
-        winningResult.put(lottoRank1);
-        winningResult.put(lottoRank2);
-        winningResult.put(lottoRank3);
-
         assertThat(winningResult.calculateTotalPrize()).isEqualTo(33_000_000);
     }
 }
