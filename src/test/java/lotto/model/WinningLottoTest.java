be7b73abfc5184ff32bfa43e5871bacc871db8cd
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 15:28:09 +0900

    test(WinningLottoTest): 테스트 주도 개발, WinningLotto 도메인에 대하여 테스트 코드 생성

---
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -0,0 +1,40 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+
+    @DisplayName("로또 당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        Lotto main = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Integer bonusNumber = 6;
+
+        assertThatThrownBy(() -> new WinningLotto(main, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 1보다 작을 경우 예외가 발생한다.")
+    @Test
+    void 보너스_번호가_1보다_작을_경우_예외가_발생한다() {
+        Lotto main = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Integer bonusNumber = 0;
+
+        assertThatThrownBy(() -> new WinningLotto(main, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 45보다 클 경우 예외가 발생한다.")
+    @Test
+    void 보너스_번호가_45보다_클_경우_예외가_발생한다() {
+        Lotto main = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Integer bonusNumber = 46;
+
+        assertThatThrownBy(() -> new WinningLotto(main, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
