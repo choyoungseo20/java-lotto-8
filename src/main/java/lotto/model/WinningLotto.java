f167586350f047828f6011077ed8c62fb4143565
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 15:31:23 +0900

    feat(WinningLotto): 보너스 번호가 1부터 45 사이의 숫자인지 판단하는 기능 구현

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -3,6 +3,7 @@ package lotto.model;
 public class WinningLotto {
 
     private static final String DUPLICATE_WINNING_LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 당첨 번호는 중복될 수 없습니다.";
+    private static final String WINNING_LOTTO_NUMBER_OUT_OF_RANGE_ERROR_MESSAGE = "[ERROR] 로또 당첨 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     private final Lotto main;
     private final Integer bonusNumber;
@@ -25,9 +26,16 @@ public class WinningLotto {
         if (isDuplicate(number)) {
             throw new IllegalArgumentException(DUPLICATE_WINNING_LOTTO_NUMBER_ERROR_MESSAGE);
         }
+        if (isOutOfRange(number)) {
+            throw new IllegalArgumentException(WINNING_LOTTO_NUMBER_OUT_OF_RANGE_ERROR_MESSAGE);
+        }
     }
 
     private boolean isDuplicate(Integer number) {
         return main.getNumbers().contains(number);
+    }
+
+    private boolean isOutOfRange(Integer number) {
+        return number < 1 || number > 45;
     }
 }
