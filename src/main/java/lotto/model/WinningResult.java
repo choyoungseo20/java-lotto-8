1a4cfa9fc7e0ebf8279f681d6e95b4838495e10b
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 16:20:57 +0900

    feat(WinningResult): HashMap을 사용하여 Rank에 해당하는 당첨 Count를 저장하는 기능 구현

---
+++ src/main/java/lotto/model/WinningResult.java
@@ -0,0 +1,18 @@
+package lotto.model;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WinningResult {
+
+    private final Map<LottoRank, Integer> rankCount = new HashMap<>();
+
+    public void put(LottoRank rank) {
+        rankCount.put(rank, rankCount.getOrDefault(rank, 0) + 1);
+    }
+
+    public Map<LottoRank, Integer> getRankCount() {
+        return rankCount;
+    }
+}
