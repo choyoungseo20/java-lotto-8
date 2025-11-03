29299675e5d40f93fa3877699cf13b26bd32b711
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 14:44:59 +0900

    test(LottoTest) 테스트 주도 개발, Lotto 도메인에 대하여 테스트 코드 작성

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,5 +21,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 1보다 작을 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_1보다_작을_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 45보다 클 경우 예외가 발생한다.")
+    @Test
+    void 로또_번호가_45보다_클_경우_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
