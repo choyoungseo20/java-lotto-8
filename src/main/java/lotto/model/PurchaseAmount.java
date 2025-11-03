73f06756a16dc1b4eb62c8d545b709a145eec88c
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 15:17:54 +0900

    feat(PurchaseAmount): 구입 금액이 1000원 단위인지 확인하는 기능 구현

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -2,13 +2,22 @@ package lotto.model;
 
 public class PurchaseAmount {
 
+    private static final String  PURCHASE_AMOUNT_INVALID_UNIT_ERROR_MESSAGE = "[ERROR] 1000원 단위로 입력해주세요.";
+
     private final Integer value;
 
     public PurchaseAmount(Integer value) {
+        validate(value);
         this.value = value;
     }
 
     public Integer getValue() {
         return value;
+    }
+
+    private void validate(Integer value) {
+        if (value % 1000 != 0) {
+            throw new IllegalArgumentException(PURCHASE_AMOUNT_INVALID_UNIT_ERROR_MESSAGE);
+        }
     }
 }
