2880d39f7a96006b3b6b69381aab756dd646c1cd
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 17:20:48 +0900

    feat(InputView): 로또 당첨 번호, 보너스 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,16 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
 
+    private static final String DELIMITER = ",";
     private static final String NUMBER_REGEX = "\\d+";
     private static final String READ_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String READ_MAIN_WINNING_LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String READ_BONUS_WINNING_LOTTO_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해 주세요.";
 
     public Integer readPurchaseAmount() {
@@ -14,8 +19,26 @@ public class InputView {
         return parseIntWithValidation(input);
     }
 
+    public List<Integer> readMainWinningLottoNumbers() {
+        System.out.println();
+        System.out.println(READ_MAIN_WINNING_LOTTO_NUMBERS_MESSAGE);
+        String input = readInput();
+        return parseMainWinningLottoNumbers(input);
+    }
+
+    public Integer readBonusWinningLottoNumber() {
+        System.out.println();
+        System.out.println(READ_BONUS_WINNING_LOTTO_NUMBER_MESSAGE);
+        String input = readInput();
+        return parseIntWithValidation(input);
+    }
+
     private String readInput() {
         return Console.readLine();
+    }
+
+    private List<Integer> parseMainWinningLottoNumbers(String input) {
+        return Arrays.stream(input.split(DELIMITER)).map(this::parseIntWithValidation).toList();
     }
 
     private Integer parseIntWithValidation(String input) {
