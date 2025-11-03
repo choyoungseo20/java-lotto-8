bc8778e45421be12cb8fb3772626915c821c4531
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 16:19:06 +0900

    test(WinningResultTest): 테스트 주도 개발, WinningResult 도메인에 대하여 테스트 코드 생성

---
+++ src/test/java/lotto/model/WinningResultTest.java
@@ -0,0 +1,46 @@
+package lotto.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import lotto.model.generator.NumberGenerator;
+import lotto.model.generator.UniqueRandomNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningResultTest {
+
+    @DisplayName("당첨 개수가 맞는지 확인한다.")
+    @Test
+    void 당첨_개수가_맞는지_확인한다() {
+        WinningResult winningResult = new WinningResult();
+
+        LottoRank lottoRank1 = LottoRank.of(5, false);
+        LottoRank lottoRank2 = LottoRank.of(5, true);
+        LottoRank lottoRank3 = LottoRank.of(5, false);
+
+        winningResult.put(lottoRank1);
+        winningResult.put(lottoRank2);
+        winningResult.put(lottoRank3);
+
+        assertThat(winningResult.getCountByRank(LottoRank.FIRST)).isEqualTo(0);
+        assertThat(winningResult.getCountByRank(LottoRank.SECOND)).isEqualTo(1);
+        assertThat(winningResult.getCountByRank(LottoRank.THIRD)).isEqualTo(2);
+    }
+
+    @DisplayName("총 당첨 금액이 맞는지 확인한다.")
+    @Test
+    void 총_당첨_금액이_맞는지_확인한다() {
+        WinningResult winningResult = new WinningResult();
+
+        LottoRank lottoRank1 = LottoRank.of(5, false);
+        LottoRank lottoRank2 = LottoRank.of(5, true);
+        LottoRank lottoRank3 = LottoRank.of(5, false);
+
+        winningResult.put(lottoRank1);
+        winningResult.put(lottoRank2);
+        winningResult.put(lottoRank3);
+
+        assertThat(winningResult.calculateTotalPrize()).isEqualTo(33_000_000);
+    }
+}
