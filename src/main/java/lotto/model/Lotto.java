1c1e27a7c11986582704f74b9a42ca45f38fd9bc
choyoungseo20 <whdudtj0809@naver.com>
2025-11-03 14:47:36 +0900

    feat(Lotto): 로또 번호가 1부터 45 사이의 숫자인지 판단하는 기능 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
 
     private static final String LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
     private static final String DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+    private static final String LOTTO_NUMBER_OUT_OF_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     private final List<Integer> numbers;
 
@@ -27,10 +28,17 @@ public class Lotto {
         if (isDuplicate(numbers)) {
             throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_ERROR_MESSAGE);
         }
+        if (isOutOfRange(numbers)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_OUT_OF_RANGE_ERROR_MESSAGE);
+        }
     }
 
     private boolean isDuplicate(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         return numbers.size() != uniqueNumbers.size();
+    }
+
+    private boolean isOutOfRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch(number -> number < 1 || number > 45);
     }
 }
